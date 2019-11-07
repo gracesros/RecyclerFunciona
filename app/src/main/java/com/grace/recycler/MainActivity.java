dbf69de98948e51906008d9a1a1d70a181fbef7a
grace <gracesros@hotmail.com>
2019-11-06 19:05:10 -0600

    Ya funciona amores

--- app/src/main/java/com/grace/recycler/MainActivity.java
+++ app/src/main/java/com/grace/recycler/MainActivity.java
@@ -3,21 +3,25 @@ package com.grace.recycler;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
 import com.grace.recycler.adapter.PokemonAdapter;
 import com.grace.recycler.adapter.PokemonItemListener;
 import com.grace.recycler.data.AppDataBaseSingleton;
 import com.grace.recycler.data.AppDatabase;
 import com.grace.recycler.data.PokemonShort;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements PokemonItemListener {
     private PokemonAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +32,13 @@ public class MainActivity extends AppCompatActivity implements PokemonItemListen
         adapter = new PokemonAdapter(new ArrayList<PokemonShort>(), this);
         recyclerView.setAdapter(adapter);
     }
+
     private void updatePokemonList(List<PokemonShort> pokemonShortList) {
         adapter.setItems(pokemonShortList);
     }
+
     public static final String EXTRA_DATA = "EXTRA_DATA";
+
     @Override
     public void onPokemonClicked(int position) {
         PokemonShort pokemonShort = adapter.getItem(position);
@@ -39,20 +46,32 @@ public class MainActivity extends AppCompatActivity implements PokemonItemListen
         intent.putExtra(EXTRA_DATA, pokemonShort);
         startActivity(intent);
     }
+
     public void addPokemon(View view) {
         Intent intent = new Intent(this, AddPokemonActivity.class);
         startActivity(intent);
     }
+
     private class GetPokemonsAsyncTask extends AsyncTask<Void, Void, List<PokemonShort>> {
         @Override
         protected List<PokemonShort> doInBackground(Void... voids) {
             AppDatabase db = AppDataBaseSingleton.getInstance(getApplicationContext()).appDatabase;
             return db.pokemonDao().getAll();
         }
+
         @Override
         protected void onPostExecute(List<PokemonShort> pokemonShortList) {
             super.onPostExecute(pokemonShortList);
             if (pokemonShortList != null) {
                 updatePokemonList(pokemonShortList);
             }
-        }}}
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GetPokemonsAsyncTask task = new GetPokemonsAsyncTask();
+        task.execute();
+    }
+}
